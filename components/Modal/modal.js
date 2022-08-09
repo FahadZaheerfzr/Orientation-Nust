46a6014baa3d5b99c47c270c77e229e0063b8a52
rasheedsaqib <mrasheed.bese19seecs@seecs.edu.pk>
2022-08-09 11:06:26 +0500

    v3

--- components/Modal/modal.js
+++ components/Modal/modal.js
@@ -33,31 +33,7 @@ const Modal = ({data, schools, lifeAtNUST, type}) => {
                             <h2 className='font-bold text-4xl text-white'>{data.name}</h2>
                         </div>
 
-                        <p className='p-8 text-xl'>
-                            School of Electrical Engineering and Computer Science (SEECS), formerly known as NUST
-                            Institute
-                            of Information Technology (NIIT), offers undergraduate, postgraduate, doctorate as well as
-                            certificate level programs in all domains of the fields of Computers and Technology.
-                            Currently
-                            offering bachelor degrees in Computer Science, Software and Electrical Engineering, SEECS
-                            lauds
-                            itself on the state-of-the-art teaching and training technologies as well as a diverse
-                            faculty
-                            with degrees from leading national and international universities. SEECS also prides itself
-                            in
-                            its multiple research labs, working on national and international projects in the domains of
-                            Machine Learning, Data Science, and more. Home to one of the two supercomputers in NUST
-                            H-12,
-                            SEECS consists of 3 departments, the Department of Computing, the Department of Electrical
-                            Engineering, and the Department of Humanities and Sciences. NUST Chapter of IEEE is also
-                            based
-                            in SEECS. Home to the famous dome and glass bridge, SEECS boasts of the biggest school in
-                            NUST
-                            H-12, encompassing 3 different schools, RIMMS and IAEC. SEECS also has traditional yearly
-                            events
-                            like SEECS Got Talent and Sports Gala, held shortly after the start of the new academic
-                            year.
-                        </p>
+                        <p className='p-8 text-xl'>{data.content}</p>
 
                     </div>
                 </div>
