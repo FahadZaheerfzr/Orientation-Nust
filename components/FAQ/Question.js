0c66e33ae186b30d336f6b4b290e2ca4098fd204
rasheedsaqib <mrasheed.bese19seecs@seecs.edu.pk>
2022-08-10 20:52:11 +0500

    faq card slider

--- components/FAQ/Question.js
+++ components/FAQ/Question.js
@@ -1,15 +1,19 @@
 import styles from "./faq.module.css";
 import {BsChevronLeft} from "react-icons/bs";
 import {useTheme} from "next-themes";
+import {motion} from "framer-motion";
 
 const Question = ({question, onClick, isSelected}) => {
 
     const {theme} = useTheme();
 
     return (
-        <div className={`${styles.accordionItem} border border-gray`} style={{marginBottom: '-1px'}}
+        <motion.div
+            transition={{layout: {type: "spring", duration: 1}}}
+            layout
+            className={`${styles.accordionItem} border border-gray`} style={{marginBottom: '-1px'}}
              onClick={onClick}>
-            <h2 className="mb-0 flex py-4 px-5 cursor-pointer">
+            <motion.h2 layout="position" className="mb-0 flex py-4 px-5 cursor-pointer">
                 <button
                     className={`font-semibold flex items-center w-full text-left rounded-none transition focus:outline-none ${theme === 'dark' ? 'text-white' : isSelected ? 'text-tyrian-purple' : 'text-black'}`}>
                     {question.question}
@@ -17,14 +21,16 @@ const Question = ({question, onClick, isSelected}) => {
                 <div className={`transition duration-300 ease-out ${isSelected ? 'rotate-90' : '-rotate-90'}`}>
                     <BsChevronLeft size={26} color={theme === 'dark' ? '#ffffff' : isSelected ? '#720240' : '#121212'}/>
                 </div>
-            </h2>
-            <div className={`${isSelected ? 'border-t border-gray h-auto' : 'h-0'}`}>
-                <div
-                    className={`py-4 px-5 ${isSelected ? 'block opacity-100 h-auto' : 'hidden opacity-0 h-0'}`}>
-                    {question.answer}
-                </div>
-            </div>
-        </div>
+            </motion.h2>
+            {isSelected && (
+                <motion.div initial={{opacity: 0}} animate={{opacity: 1}} className='border-t border-gray h-auto'>
+                    <div
+                        className='py-4 px-5 block opacity-100 h-auto'>
+                        {question.answer}
+                    </div>
+                </motion.div>
+            )}
+        </motion.div>
     )
 }
 
