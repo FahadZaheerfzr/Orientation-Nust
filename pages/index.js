5c7c32f783c885a6e9f6e06d70c622d286caad54
abbassays <maliabbas2001@gmail.com>
2022-08-12 04:25:02 +0500

    responsive event pics done

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,59 @@
 import SplashComponent from "../components/SplashComponent/SplashComponent";
 import MyParticles from "../components/Particles/Particles";
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import Notes from "../components/Home/Notes";
-import messages from '../util/messages.json';
-import events from '../util/events.json';
+import messages from "../util/messages.json";
+import events from "../util/events.json";
 import Events from "../components/Home/Events";
 
-const Home = ({messages, events}) => {
+const Home = ({ messages, events }) => {
+  const [click, setClick] = useState(false);
 
-    const [click, setClick] = useState(false);
+  const splashRef = useRef(null);
+  const containerRef = useRef(null);
 
-    const splashRef = useRef(null);
-    const containerRef = useRef(null);
+  useEffect(() => {
+    setTimeout(() => {
+      splashRef.current.style.display = click ? "none" : "flex";
+      containerRef.current.style.opacity = click ? 1 : 0;
+    }, 500);
+  }, [click]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            splashRef.current.style.display = click ? 'none' : 'flex';
-            containerRef.current.style.opacity = click ? 1 : 0;
-        }, 500);
-    }, [click]);
+  return (
+    <>
+      <div
+        ref={splashRef}
+        style={{ height: "80vh" }}
+        className={`flex justify-center items-center relative z-30 transition duration-500 ease-in-out ${
+          click ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <SplashComponent onClick={() => setClick(true)} />
+      </div>
 
-    return (
-        <>
+      <MyParticles />
 
-            <div ref={splashRef} style={{height: '80vh'}}
-                 className={`flex justify-center items-center relative z-30 transition duration-500 ease-in-out ${click ? 'opacity-0' : 'opacity-100'}`}>
-                <SplashComponent onClick={() => setClick(true)}/>
-            </div>
+      <div
+        ref={containerRef}
+        className={`mt-24 transition-opacity duration-500 ease-in-out ${
+          click ? "block" : "hidden"
+        }`}
+      >
+        <Notes messages={messages} />
 
-            <MyParticles/>
-
-            <div ref={containerRef}
-                 className={`mt-24 transition-opacity duration-500 ease-in-out ${click ? 'block' : 'hidden'}`}>
-
-                <Notes messages={messages}/>
-
-                <Events events={events}/>
-            </div>
-        </>
-    )
-}
+        <Events events={events} />
+      </div>
+    </>
+  );
+};
 
 export const getStaticProps = async () => {
-    return {
-        props: {
-            messages, events
-        },
-    }
-}
+  return {
+    props: {
+      messages,
+      events,
+    },
+  };
+};
 
 export default Home;
