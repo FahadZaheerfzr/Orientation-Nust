7114f7e50cb0cc5a7fa2cc0428d2f085a982e94a
Hamid Muzaffar <hamidmuzaffar49@gmail.com>
2022-09-01 00:50:40 +0500

    inital commit

--- pages/history.js
+++ pages/history.js
@@ -1,17 +1,13 @@
 import Layout from "../components/Layout";
 import React from "react";
 import Desktopflipbook from "../components/FlipBook/Desktopflipbook";
-import Mobileflipbook from "../components/FlipBook/Mobileflipbook";
 import ON from '../util/history.json'
-import { useMediaQuery } from "react-responsive";
 
-const History = ({ coverpage, history, lastpage }) => {
-        const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1024px)'})
-      //  const content = isDesktopOrLaptop ?  : 
+const History = ({ coverpage, history, lastpage }) => { 
         return (
                 <>
                         <Layout>
-                        <div className="overflow-hidden mt-16 md:mt-0 celebration-div">
+                        <div className="overflow-hidden mt-16 md:mt-0 celebration-div pt-4">
                                 <Desktopflipbook coverpage={coverpage} history={history} lastpage={lastpage}></Desktopflipbook>
                         </div>
                         
