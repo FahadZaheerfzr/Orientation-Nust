dfe084864e8e19a855ea8af9562cf729ea448946
Hamid Muzaffar <hamidmuzaffar49@gmail.com>
2022-09-05 00:10:54 +0500

    Content Added

--- util/merchandise.js
+++ util/merchandise.js
@@ -5,46 +5,20 @@ const products = {
         {
             id: 1,
             name: 'Angled view',
-            src: '/logo/logo2017.jpg',
+            src: '/Merch/front.jpg',
             alt: 'Angled front view with bag zipped and handles upright.',
         },
         {
             id: 2,
             name: 'Angled view',
-            src: '/logo/logo2018.svg',
+            src: '/Merch/back.jpg',
             alt: 'Angled front view with bag zipped and handles upright.',
         }],
-    [
-        {
-            id: 1,
-            name: 'Angled view',
-            src: '/logo/logo2019.jpg',
-            alt: 'Angled front view with bag zipped and handles upright.',
-        },
-        {
-            id: 2,
-            name: 'Angled view',
-            src: '/logo/logo2020.jpg',
-            alt: 'Angled front view with bag zipped and handles upright.',
-        }],
-    [
-        {
-            id: 1,
-            name: 'Angled view',
-            src: '/logo/logo2020.jpg',
-            alt: 'Angled front view with bag zipped and handles upright.',
-        },
-        {
-            id: 2,
-            name: 'Angled view',
-            src: '/logo/logo2021.jpg',
-            alt: 'Angled front view with bag zipped and handles upright.',
-        }]
     ],
     colors: [
-        { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700', index: 0 },
-        { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400', index: 1 },
-        { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500', index: 2 }
+        { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700'},
+        { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400'},
+        { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500'}
     ],
     description: `
 <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
